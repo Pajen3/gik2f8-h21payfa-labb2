016be09befeab725f93ae662715ab005b3354074
Pajen3 <95629241+Pajen3@users.noreply.github.com>
2022-12-06 11:19:25 +0100

    delete

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app
 .use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
-    res.header("Access-Controll-Allow-Method", "*");
+    res.header("Access-Control-Allow-Methods", "*");
 
     next();
 });
@@ -69,3 +69,5 @@ app.delete("/tasks/:id", async(req, res) => {
 });
 
 app.listen(PORT, () => console.log("Server running on http://localhost:5000"));
+
+
